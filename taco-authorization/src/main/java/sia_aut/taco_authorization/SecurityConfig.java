bfad7da0c0369f0dc37efd06d0352b8c4c8a5b99
lexYus <yusupov-alex.i@yandex.ru>
2026-06-04 21:08:51 +0500

    исправление метода formLogin

--- taco-authorization/src/main/java/sia_aut/taco_authorization/SecurityConfig.java
+++ taco-authorization/src/main/java/sia_aut/taco_authorization/SecurityConfig.java
@@ -1,6 +1,7 @@
 package sia_aut.taco_authorization;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,7 +15,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(aut -> aut
                         .anyRequest().authenticated());
-        http.formLogin();
+        http.formLogin(Customizer.withDefaults());
         return http.build();
     }
 
